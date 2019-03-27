47e4c05917f5b49d4a52d66757a60c91f79568f4
Dror Elovits <dror.elovits@gmail.com>
2019-03-27 22:24:31 +0200

    Readded firebase.js

---
+++ plugins/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/database'
+
+if (!firebase.apps.length) {
+  const config = {
+    apiKey: 'AIzaSyCaz5bgRphl_vfp_6gCAtMu8lMSN8ey-Qs',
+    authDomain: 'tikkoun-sofrim.firebaseapp.com',
+    databaseURL: 'https://tikkoun-sofrim.firebaseio.com',
+    projectId: 'tikkoun-sofrim',
+    storageBucket: 'tikkoun-sofrim.appspot.com',
+    messagingSenderId: '124038584312'
+  }
+
+  firebase.initializeApp(config)
+}
+
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
+export const auth = firebase.auth()
+export const DB = firebase.database()
+export const StoreDB = firebase.firestore()
+export default firebase
