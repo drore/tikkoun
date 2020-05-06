0d0498b7078e823c30cc83e473d45272ca0027a1
Dror Elovits <dror.elovits@gmail.com>
2020-05-06 10:55:47 +0300

    WIP new design

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -2,5 +2,6 @@ import Vue from 'vue'
 import VueGtag from "vue-gtag";
 
 Vue.use(VueGtag, {
-  config: { id: "UA-133908181-1" }
+  //config: { id: "UA-133908181-1" } This is for the "old" tikkoun sofrim site
+  config: {id: "UA-165125436-1"}
 });
